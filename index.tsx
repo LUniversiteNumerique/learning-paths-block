0eb0c48c496dcd21f5548ee00eaf04062d353ba9
Pierre <pierre.duverneix@gmail.com>
2021-09-22 16:33:06 +0200

    working edit/save

--- index.tsx
+++ index.tsx
@@ -3,36 +3,24 @@ import { registerBlockType } from '@wordpress/blocks';
 import Edit from './src/components/edit';
 import save from './src/components/save';
 
-interface Attributes {
-    content: string | unknown,
-	//data: { type: string; default: ''; }
-}
-
-interface EditProps {
-	[key: string]: unknown;
-	className: string;
-	attributes: Attributes;
-	setAttributes: (attributes: any) => void;
-}
-
 registerBlockType('learningpathsblock/learningpathsblock-esnext', {
     title: 'Learning Paths Block',
     icon: 'universal-access-alt',
     category: 'design',
     attributes: {
-        content: "string",
-        /*data: {
+        content: {
             type: "string",
-            default: ""
-        }*/
+            default: "hello world"
+        }
     },
-    edit: ({ attributes, setAttributes }) => <Edit attributes={attributes} setAttributes={setAttributes} />,
+    edit: (props) => <Edit {...props} />,
     save: (props) => {
         let { attributes: { content } } = props;
-        console.log("saved :::: ", content);
+        // @ts-expect-error The type given is more strict than the definition
+        let ct: string = content;
         return (
           <div>
-            <p>"test"</p>
+            <p>{ ct }</p>
           </div>
         )
       }
--- src/components/edit.tsx
+++ src/components/edit.tsx
@@ -3,7 +3,7 @@ import apiFetch from '@wordpress/api-fetch';
 import { RichText  } from '@wordpress/block-editor';
 
 interface Attributes {
-    content: string | unknown;
+    content: string | any;
 	//data: { type: string; default: ''; }
 }
 
@@ -18,15 +18,12 @@ const Edit: FunctionComponent<EditProps> = ({ ...props }: EditProps) => {
     const [error, setError]         = useState<any>(null);
     const [data, setData]           = useState<any>(null);
     const [isLoaded, setIsLoaded]   = useState(false);
-    const [content, setContent]     = useState('');
 
     useEffect(() => {
         apiFetch({ url: `${endpoint}/diploma/1` }).then(
             (result) => {
                 setIsLoaded(true);
                 setData(result);
-                setContent('blockc ontent !');
-                console.log("props : ", props);
             },
             (error) => {
                 setIsLoaded(true);
@@ -34,33 +31,23 @@ const Edit: FunctionComponent<EditProps> = ({ ...props }: EditProps) => {
             }
         );
     }, []);
-
+    
     if (error) {
         return <p>Error : { error.message }</p>;
     } else if (! isLoaded) {
         return <p>Loading data...</p>;
     } else if (data) {
-        console.log("my data : ", data);
-        console.log("my data name : ", data?.name);
+        props.setAttributes({ content: data?.name });
         return (
             <div>
                 <RichText 
                     className="example-content"
-                    value={content}
-                    onChange={(content) => props.setAttributes({ content })} />
+                    value={data?.name}
+                    onChange={(_) => props.setAttributes({ content: data?.name })} />
             </div>
         )
-        //return <h3>Data : <i>{ data?.name }</i> loaded!</h3>;
     }
-
-	return (
-        <div>
-            <RichText 
-                className="example-content"
-                value={content}
-                onChange={(content) => props.setAttributes({ content })} />
-        </div>
-    );
+	return <div>...</div>;
 }
 
 export default Edit;
