faac25dec94fd79483f9e68b17eb2d1649af1718
Pierre <pierre.duverneix@gmail.com>
2021-09-24 16:03:50 +0200

    build view

--- src/components/edit.tsx
+++ src/components/edit.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, FunctionComponent } from 'react';
 import apiFetch from '@wordpress/api-fetch';
+import DataView from './DataView';
 
 interface Attributes {
     content: string | any;
@@ -33,43 +34,14 @@ const Edit: FunctionComponent<EditProps> = ({ ...props }: EditProps) => {
         return <p>Error : { error.message }</p>;
     } else if (! isLoaded) {
         return <p>Loading data...</p>;
-    } else if (data && data.diplomas[0]) {
+    } else if (data && data.fields[0]) {
         props.setAttributes({ 
-            content: data.diplomas, 
+            content: data.fields,
         } as any);
-
-        const content = data?.diplomas;
-
-        return (
-            <div className="lpb-container">
-                <ul className="lpb-diploma-list">
-                    { content && content.map((row : any) => {
-                        return (
-                            <article className="diploma">
-                                <h3 className="diploma-name">{row.name}</h3>
-                                { row.years && row.years.map((year : any) => {
-                                    return (
-                                        <>
-                                            <h4 className="year-name">{year.name}</h4>
-                                            { row.ue && row.ue.map((ue : any) => {
-                                                return (
-                                                    <h5 className="ue-name">{ue.name}</h5>
-                                                )
-                                            }) }
-                                        </>
-                                    )
-                                }) }
-                                <section className="lpb-body-data">
-                                    Ce parcours d’informatique générale permet d’hybrider les enseignements des domaines suivants : concepts informatiques, algorithmique et programmation, systèmes d’exploitation, outils et technologies Internet, bases de données. Il propose également une composante mathématique importante.
-                                </section>
-                            </article>
-                        )
-                    }) }
-                </ul>
-                <section className="lpb-body"></section>
-            </div>
-        )
+        const content = data?.fields as Array<{}>;
+        return ( <DataView content={content} /> );
     }
+
 	return <div>...</div>;
 }
 
--- src/components/save.tsx
+++ src/components/save.tsx
@@ -1,37 +1,9 @@
 import * as React from 'react';
+import DataView from './DataView';
 
 const Save = ({ ...props }: any) => {
     const content = props.attributes.content;
-    
-    return (
-        <div className="lpb-container">
-            <ul className="lpb-diploma-list">
-                { content && content.map((row : any) => {
-                    return (
-                        <article className="diploma">
-                            <h3 className="diploma-name">{row.name}</h3>
-                            { row.years && row.years.map((year : any) => {
-                                return (
-                                    <>
-                                        <h4 className="year-name">{year.name}</h4>
-                                        { row.ue && row.ue.map((ue : any) => {
-                                            return (
-                                                <h5 className="ue-name">{ue.name}</h5>
-                                            )
-                                        }) }
-                                    </>
-                                )
-                            }) }
-                            <section className="lpb-body-data">
-                                Ce parcours d’informatique générale permet d’hybrider les enseignements des domaines suivants : concepts informatiques, algorithmique et programmation, systèmes d’exploitation, outils et technologies Internet, bases de données. Il propose également une composante mathématique importante.
-                            </section>
-                        </article>
-                    )
-                }) }
-            </ul>
-            <section className="lpb-body"></section>
-        </div>
-    )
+    return <DataView {...content} />;
 };
 
 export default Save;
