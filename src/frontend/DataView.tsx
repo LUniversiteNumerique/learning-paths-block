5044d85b0c2e3035caaaf8de9cb614f0c2a8f0a7
Pierre <pierre.duverneix@gmail.com>
2021-10-04 16:45:11 +0200

    build tsx data view

--- src/frontend/DataView.tsx
+++ src/frontend/DataView.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { baseURI } from '../utils/utils';
+import strings from '../utils/strings.utils';
+import { Resource } from '../types/Resource';
+import { createRow, createHeader } from '../utils/table.utils';
 
 const DataView = () => {
-    const [html, setHTML] = useState<JSX.Element>(<React.Fragment></React.Fragment>);
-    const [diplomas, setDiplomas] = useState<NodeListOf<Element>>();
+    const [html, setHTML] = useState<JSX.Element[]>([]);
     const modal = document.getElementById('lpb-modal') as HTMLElement;
     const closeBtn = document.getElementById('lpb-modal-close') as HTMLElement;
 
@@ -19,9 +21,9 @@ const DataView = () => {
 
     const buildHTML = (data?: JSX.Element) => {
         if (data === undefined) {
-            setHTML(<React.Fragment></React.Fragment>);
+            setHTML([]);
         } else {
-            setHTML((rest: any) => rest + data);
+            setHTML(rest => [...rest, data]);
         }
     }
 
@@ -39,14 +41,40 @@ const DataView = () => {
         if (parent) {
             parent.classList.toggle('active');
             buildHTML(); // Clear the html
-
+            //buildHTML(<p>test</p>);
             fetch(`${baseURI}/wp-content/plugins/learning-paths-api/api.php/diploma/${diplomaId}`)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
-                    if (data.description) {
-                        buildHTML(<p>{data.description}</p>);
-                    }
+                    const out = <article className="lpb-diploma">
+                                    <h4 className="lpb-diploma-name">{data.name}</h4>
+                                    <div className="lpb-diploma-description" dangerouslySetInnerHTML={{ __html: data.description }} />
+                                    <section className="lpb-diploma-body">
+                                    { data.years && data.years.map((year : any) => {
+                                        return (
+                                            <>
+                                                <h5>{year.name}</h5>
+                                                <table>
+                                                    <thead>
+                                                        <tr>{createHeader(strings.thead)}</tr>
+                                                    </thead>
+                                                    { year.ue && year.ue.map((ue : any) => {
+                                                        return ue.resources 
+                                                            ? <tbody>
+                                                                { ue.resources 
+                                                                    && ue.resources.map((resource: Resource, i: number): JSX.Element => {
+                                                                    return createRow(resource, ue, "resource", ue.resources.length, i)
+                                                                }) }
+                                                            </tbody> 
+                                                            : null
+                                                    }) }
+                                                </table>
+                                            </>
+                                        )
+                                    }) }
+                                    </section>
+                                </article>;
+                    buildHTML(<>{out}</>);
                 })
                 .catch(console.error);
     
@@ -69,7 +97,13 @@ const DataView = () => {
         }
     }
 
-    return html;
+    return(
+        <div>
+            { Object.values(html).map((element, index) => {
+                return <div key={index}>{element}</div>;
+            }) }
+        </div>
+    );
 };
 
 export default DataView;
