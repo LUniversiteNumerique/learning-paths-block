ee549b38a8aa829a40b036a8cb5ed4997d81334d
Pierre <pierre.duverneix@gmail.com>
2021-10-14 14:41:06 +0200

    refactor DataView

--- src/components/DataView.tsx
+++ src/components/DataView.tsx
@@ -1,32 +1,15 @@
 import * as React from 'react';
+import Modal from './Modal';
+import Field from './Field';
 
 const DataView = (props : any) => {
     const content = props.content;
     return (
         <div className="lpb-container">
-            <div id="lpb-frontend-receiver" />
-            <div id="lpb-modal">
-                <div id="lpb-modal-content">
-                    <span id="lpb-modal-close">&times;</span>
-                    <div id="lpb-modal-content-body">...</div>
-                </div>
-            </div>
+            <Modal />
             <ul className="lpb-diploma-list">
                 { content && content.map((row : any) => {
-                    return (
-                        <section className="lpb-field">
-                            <h3 className="lpb-field-name">{row.name}</h3>
-                            <div className="lpb-field-content">
-                                { row.diplomas && row.diplomas.map((diploma : any) => {
-                                    return (
-                                        <article className="lpb-diploma">
-                                            <h4 className="lpb-diploma-name" data-lpb-id={diploma.id}>{diploma.name}</h4>
-                                        </article>
-                                    )
-                                }) }
-                            </div>
-                        </section>
-                    )
+                    return (<Field {...row} />);
                 }) }
             </ul>
         </div>
---
+++ src/components/Field.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Field as FieldProps } from '../types/Field';
+import Diploma from './Diploma';
+
+const Field = (field: FieldProps): JSX.Element => {
+    return (
+        <section className="lpb-field">
+            <h3 className="lpb-field-name">{field.name}</h3>
+            <div className="lpb-field-content">
+                { field.diplomas && field.diplomas.map((diploma : any) => {
+                    return (<Diploma {...diploma} />);
+                }) }
+            </div>
+        </section>
+    );
+};
+
+export default Field;
