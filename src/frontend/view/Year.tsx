b21e17e72147fc6d246d52d9c811213e966e8a64
Pierre <pierre.duverneix@gmail.com>
2021-10-15 11:57:18 +0200

    display flex-table onClick

--- src/frontend/view/Year.tsx
+++ src/frontend/view/Year.tsx
@@ -1,12 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Ue from './Ue';
 import type { YearData } from '../../types/Data';
 
 const Year = (year: YearData) => {
+    const [selected, setSelected] = useState<boolean>(false);
+
+    const handleSelection = () => {
+        setSelected(true);
+    };
+
     return (
         <>
-            <h5 className="lpb-year-name">{year.name}</h5>
-            <div className="flex-table">
+            <h5 className="lpb-year-name" onClick={() => handleSelection()}>
+                {year.name}
+            </h5>
+            <div className={`flex-table ${selected ? 'active' : ''}`}>
                 { year.ue && year.ue.map((ue : any) => {
                     return ue.resources && ue.name
                         ?   <Ue {...ue} /> 
