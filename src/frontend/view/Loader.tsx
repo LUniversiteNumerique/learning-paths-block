e2b55c0f982544b779c9c387c1545c669b79d185
Pierre <pierre.duverneix@gmail.com>
2021-10-18 15:38:50 +0200

    add css loader

--- src/frontend/view/Loader.tsx
+++ src/frontend/view/Loader.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 const Loader = (): JSX.Element => {
     return (
         <div className="loader">
-            ...
+            <div className="loader-inner" />
+            <h6>Chargement</h6>
         </div> 
     );
 };
