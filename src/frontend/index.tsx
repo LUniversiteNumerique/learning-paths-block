16137d285b7c964f2f7fe7a3c81b42c1cafc78a9
ledob44 <olivier.imbert@unit.eu>
2026-01-22 15:56:30 +0100

    error

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -4,11 +4,14 @@ import AppProvider from './components/AppProvider';
 import App from './components/App';
 import { baseURI } from '../utils/utils';
 
-if (document.getElementById("learningpaths-block-root") !== null) {
+if (document.getElementById("learningpaths-block-root")) {
+    console.log('exists');
     WPElement.render(
         <AppProvider>
             <App apiUrl={baseURI}/>
         </AppProvider>,
         document.getElementById('learningpaths-block-root')
     );
+}else{
+    console.log('nope');
 }
