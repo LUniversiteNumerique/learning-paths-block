20630ac77eb11f2419587a83d8e2e68c79cd07de
ledob44 <olivier.imbert@unit.eu>
2026-01-22 15:52:33 +0100

    error

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -4,10 +4,11 @@ import AppProvider from './components/AppProvider';
 import App from './components/App';
 import { baseURI } from '../utils/utils';
 
-
-WPElement.render(
-    <AppProvider>
-        <App apiUrl={baseURI} />
-    </AppProvider>,
-    document.getElementById('learningpaths-block-root')
-);
+if (document.getElementById("learningpaths-block-root")) {
+    WPElement.render(
+        <AppProvider>
+            <App apiUrl={baseURI}/>
+        </AppProvider>,
+        document.getElementById('learningpaths-block-root')
+    );
+}
